529bfca95abd6f79f00ee2d25c3acd8f00e1a3c0
linshengwang <lsw957859199@163.com>
2018-08-28 11:17:51 +0800

    fix

--- issue-service/src/main/java/cn/edu/fudan/issueservice/dao/IssueTypeDao.java
+++ issue-service/src/main/java/cn/edu/fudan/issueservice/dao/IssueTypeDao.java
@@ -2,12 +2,18 @@ package cn.edu.fudan.issueservice.dao;
 
 import cn.edu.fudan.issueservice.domain.IssueType;
 import cn.edu.fudan.issueservice.mapper.IssueTypeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class IssueTypeDao {
 
     private IssueTypeMapper issueTypeMapper;
+
+    @Autowired
+    public void setIssueMapper(IssueTypeMapper issueTypeMapper) {
+        this.issueTypeMapper = issueTypeMapper;
+    }
 
     public IssueType getIssueTypeByTypeName(String type){
         return  issueTypeMapper.getIssueTypeByTypeName(type);
