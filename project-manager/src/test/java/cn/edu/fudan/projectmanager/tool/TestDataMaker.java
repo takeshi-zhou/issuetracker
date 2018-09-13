90301c54159a37de251152e8611e92c03b9b1a38
unknown <18212010063@fudan.edu.cn>
2018-09-13 21:50:15 +0800

    fix test

--- project-manager/src/test/java/cn/edu/fudan/projectmanager/tool/TestDataMaker.java
+++ project-manager/src/test/java/cn/edu/fudan/projectmanager/tool/TestDataMaker.java
@@ -25,13 +25,28 @@ public class TestDataMaker {
         project.setUuid("pro2");
         project.setName("Java");
         project.setLanguage("Java");
-        project.setUrl("https://github.com/DuGuQiuBai/Java");
+        project.setUrl("https://github.com/spotify/docker-maven-plugin");
         project.setVcs_type("git");
         project.setAccount_id("1");
         project.setDownload_status("Downloaded");
         project.setScan_status("Not Scanned");
         project.setDescription("27天成为Java大神");
-        project.setRepo_id("227a91de-a522-11e8-8fa0-d067e5ea858d");
+        project.setRepo_id("RepoId");
+        return project;
+    }
+
+    public  Project projectMakerPro1Update(){
+        Project project = new Project();
+        project.setUuid("pro1");
+        project.setName("UpdateData");
+        project.setLanguage("Java");
+        project.setUrl("https://github.com/spotify/docker-maven-plugin");
+        project.setVcs_type("git");
+        project.setAccount_id("1");
+        project.setDownload_status("Downloaded");
+        project.setScan_status("Not Scanned");
+        project.setDescription("27天成为Java大神");
+        project.setRepo_id("RepoId");
         return project;
     }
 }
