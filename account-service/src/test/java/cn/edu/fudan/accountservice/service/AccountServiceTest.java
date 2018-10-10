1997cec73f33b01a18c94e61370257ca574f36ae
unknown <18212010063@fudan.edu.cn>
2018-10-10 15:14:16 +0800

    unit test

--- account-service/src/test/java/cn/edu/fudan/accountservice/service/AccountServiceTest.java
+++ account-service/src/test/java/cn/edu/fudan/accountservice/service/AccountServiceTest.java
@@ -1,124 +1,223 @@
 package cn.edu.fudan.accountservice.service;
 
 import cn.edu.fudan.accountservice.AccountServiceApplicationTests;
+import cn.edu.fudan.accountservice.dao.AccountDao;
 import cn.edu.fudan.accountservice.domain.Account;
+import cn.edu.fudan.accountservice.domain.AccountInfo;
 import cn.edu.fudan.accountservice.domain.ResponseBean;
-import cn.edu.fudan.accountservice.tool.MockTestConnection;
-import org.junit.*;
+import cn.edu.fudan.accountservice.service.impl.AccountServiceImpl;
+import cn.edu.fudan.accountservice.tool.TestDataMaker;
+import cn.edu.fudan.accountservice.util.MD5Util;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.api.support.membermodification.MemberModifier;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 
+@PrepareForTest({AccountService.class, AccountServiceImpl.class, AccountDao.class})
 public class AccountServiceTest extends AccountServiceApplicationTests {
 
+    @Mock
+    private AccountDao accountDao;
+
     @Autowired
-    AccountService accountService;
+    @InjectMocks
+    private AccountService accountService = new AccountServiceImpl();
 
-    public static MockTestConnection mockTestConnection;
+    private ResponseBean responseBean;
+    private Account account;
 
-    @BeforeClass
-    public static void setupConnection() throws Exception {
-        mockTestConnection = new MockTestConnection();
-        mockTestConnection.setupCoon();
-    }
 
     @Before
     public void setupData() throws Exception {
-        String sql = "insert into account values(\"3\",\"admin\",\"f6fdffe48c908deb0f4c3bd36c032e72\",\"admin\",\"123456@fudan.edu.cn\")";
-        Statement stmt = mockTestConnection.getConn().createStatement();//创建一个Statement对象
-        stmt.executeUpdate(sql);//执行sql语句
+        responseBean = new ResponseBean();
+        account = TestDataMaker.accountMaker();
+        MemberModifier.field(AccountServiceImpl.class, "accountDao").set(accountService, accountDao);
         System.out.println("finish mocking");
     }
 
 
     @Test
     public void login() {
+
         /*
             正确的用户名及密码
          */
-        ResponseBean responseBeanCorrect = accountService.login("admin", "admin");
-        System.out.println(responseBeanCorrect.toString());
+        String username = "admin";
+        String password = "admin";
+        String encodePassword = MD5Util.md5(username + password);
+        responseBean.setCode(200);
+        responseBean.setData(new AccountInfo(username,  MD5Util.md5(encodePassword)));
+        responseBean.setMsg("登录成功!");
+        PowerMockito.when(accountDao.login(username,encodePassword)).thenReturn(account);
+        ResponseBean responseBeanCorrect = accountService.login(username, password);
+        Assert.assertEquals(responseBean.getCode(), responseBeanCorrect.getCode());
 
         /*
             错误的用户名及密码
          */
-        ResponseBean responseBeanIncorrect = accountService.login("admin", "admins");
-        System.out.println(responseBeanIncorrect.toString());
+        username = "admin";
+        password = "admins";
+        responseBean.setCode(401);
+        responseBean.setData(null);
+        responseBean.setMsg("用户名或密码错误!");
+        encodePassword = MD5Util.md5(username + password);
+        PowerMockito.when(accountDao.login(username,encodePassword)).thenReturn(null);
+        ResponseBean responseBeanIncorrect = accountService.login(username, password);
+        Assert.assertEquals(responseBean.getCode(), responseBeanIncorrect.getCode());
     }
 
     @Test
-    public void getUserNameByToken() {
-        /*
-            正确的token
-         */
-        String usernameCorrect = accountService.getUserNameByToken("ec15d79e36e14dd258cfff3d48b73d35");
-        System.out.println(usernameCorrect);
-
-        /*
-            错误的token
-         */
-        //String usernameIncorrect = accountService.getUserNameByToken("123"); // 错误的token
-        //System.out.println(usernameCorrect);
-    }
-
-    @Test
-    public void getAccountByToken() {
-        Account account = accountService.getAccountByToken("ec15d79e36e14dd258cfff3d48b73d35");
-        if (account != null) {
-            System.out.println(account.getAccountName() + " " + account.getPassword());
-        } else {
-            System.out.println("token输入错误");
-        }
-
-
-    }
-
-//    @Test
-//    public void getAccountIdByAccountName() {
-//        /*
-//            正确的AccountName
-//         */
-//        String idCorrect  = accountService.getAccountIdByAccountName("admin");
-//        System.out.println(idCorrect);
-//
-//        /*
-//            错误的AccountName
-//         */
-//        String idIncorrect  = accountService.getAccountIdByAccountName("admins");
-//        System.out.println(idIncorrect);
-//    }
-
     @Ignore
+    public void getUserNameByToken() {
+
+    }
+
     @Test
-    @Transactional
-    public void addAccount() {
+    @Ignore
+    public void getAccountByToken() {
+
+    }
+
+    @Test
+    @Ignore
+    public void authByToken() {
+
+    }
+
+    @Test
+    public void addAccountTest_Null() {
+        account.setName(null);
+        try{
+            accountService.addAccount(account);
+        }catch(Exception e){
+            Assert.assertEquals("param loss",e.getMessage());
+        }
+    }
+
+    @Test
+    public void addAccountTest_AccountName() {
+        PowerMockito.when(accountService.isAccountNameExist(account.getAccountName())).thenReturn(true);
+        try{
+            accountService.addAccount(account);
+        }catch(Exception e){
+            Assert.assertEquals("accountName has been used!",e.getMessage());
+        }
+    }
+
+    @Test
+    public void addAccountTest_Email() {
+        PowerMockito.when(accountService.isAccountNameExist(account.getAccountName())).thenReturn(false);
+        PowerMockito.when(accountService.isEmailExist(account.getEmail())).thenReturn(true);
+        try{
+            accountService.addAccount(account);
+        }catch(Exception e){
+            Assert.assertEquals("email has been used!",e.getMessage());
+        }
+    }
+
+    @Test
+    public void addAccountTest_Name() {
+        PowerMockito.when(accountService.isAccountNameExist(account.getAccountName())).thenReturn(false);
+        PowerMockito.when(accountService.isEmailExist(account.getEmail())).thenReturn(false);
+        PowerMockito.when(accountService.isNameExist(account.getName())).thenReturn(true);
+        try{
+            accountService.addAccount(account);
+        }catch(Exception e){
+            Assert.assertEquals("nickName has been used!",e.getMessage());
+        }
+    }
+
+    @Test
+    public void addAccountTest_Success() {
+        PowerMockito.when(accountService.isAccountNameExist(account.getAccountName())).thenReturn(false);
+        PowerMockito.when(accountService.isEmailExist(account.getEmail())).thenReturn(false);
+        PowerMockito.when(accountService.isNameExist(account.getName())).thenReturn(false);
+        doNothing().when(accountDao).addAccount(account);
+        try{
+            accountService.addAccount(account);
+        }catch(Exception e){
+        }
+        verify(accountDao, Mockito.atLeast(1)).addAccount(account);
     }
 
     @Test
     public void getAllAccountId() {
-        List<String> listString = accountService.getAllAccountId();
-        for (String id : listString) {
-            System.out.println(id);
+        List<String> list = new ArrayList<String>();
+        list.add("1");
+        list.add("2");
+        PowerMockito.when(accountDao.getAllAccountId()).thenReturn(list);
+        List<String> listResult = accountService.getAllAccountId();
+        Assert.assertEquals(list.size(), listResult.size());
+        for(int i=0;i<listResult.size();i++){
+            Assert.assertEquals(list.indexOf(i), listResult.indexOf(i));
         }
     }
 
+    @Test
+    public void isAccountNameExist() {
+        /*
+            accountName 已存在时
+         */
+        String accountName = "admin";
+        PowerMockito.when(accountDao.isAccountNameExist(accountName)).thenReturn(true);
+        Boolean result = accountService.isAccountNameExist(accountName);
+        Assert.assertEquals(true, result);
 
-    @After
-    public void cleanData() throws Exception {
-        String sql = "delete from account where uuid='3'";
-        Statement stmt = mockTestConnection.getConn().createStatement();//创建一个Statement对象
-        stmt.executeUpdate(sql);//执行sql语句
-        System.out.println("finish cleaning");
+        /*
+            accountName 不存在时
+         */
+        PowerMockito.when(accountDao.isAccountNameExist(accountName)).thenReturn(false);
+        result = accountService.isAccountNameExist(accountName);
+        Assert.assertEquals(false, result);
     }
 
-    @AfterClass
-    public static void closeConnection() throws Exception {
-        mockTestConnection.closeCoon();
-        System.out.println("关闭数据库成功");
+    @Test
+    public void isNameExist() {
+        /*
+            Name 已存在时
+         */
+        String name = "acc";
+        PowerMockito.when(accountDao.isNameExist(name)).thenReturn(true);
+        Boolean result = accountService.isNameExist(name);
+        Assert.assertEquals(true, result);
+
+        /*
+            Name 不存在时
+         */
+        PowerMockito.when(accountDao.isNameExist(name)).thenReturn(false);
+        result = accountService.isNameExist(name);
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void isEmailExist() {
+        /*
+            Email 已存在时
+         */
+        String email = "123@qq.com";
+        PowerMockito.when(accountDao.isEmailExist(email)).thenReturn(true);
+        Boolean result = accountService.isEmailExist(email);
+        Assert.assertEquals(true, result);
+
+        /*
+            Email 不存在时
+         */
+        PowerMockito.when(accountDao.isEmailExist(email)).thenReturn(false);
+        result = accountService.isEmailExist(email);
+        Assert.assertEquals(false, result);
     }
 
 }
