b9f9937dc68df93b52e84bb28101f1a2e7d260e1
wzy <2410685592@qq.com>
2018-10-16 14:57:38 +0800

    sort tag

--- tag-service/src/main/java/cn/edu/fudan/tagservice/domain/Priority.java
+++ tag-service/src/main/java/cn/edu/fudan/tagservice/domain/Priority.java
@@ -4,16 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum Priority {
-    Immediate("#b52a28"),
-    Urgent("#e36e6c"),
-    High("#eba47c"),
-    Normal("#ebcb8d"),
-    Low("#a0b08f"),
-    Ignore("#628e8b"),
-    Solved("#d3d3d3"),
-    Misinformation("#5499bd");
+    Immediate("#b52a28",7),
+    Urgent("#e36e6c",6),
+    High("#eba47c",5),
+    Normal("#ebcb8d",4),
+    Low("#a0b08f",3),
+    Ignore("#628e8b",2),
+    Solved("#d3d3d3",1),
+    Misinformation("#5499bd",0);
 
     private String color;
+    private int level;
     private static Map<String, Priority> valueMap = new HashMap<>();
 
     static {
@@ -22,8 +23,9 @@ public enum Priority {
         }
     }
 
-    Priority(String color) {
+    Priority(String color,int level) {
         this.color = color;
+        this.level=level;
     }
 
     public static boolean contains(String name) {
@@ -32,6 +34,10 @@ public enum Priority {
 
     public String getColor() {
         return color;
+    }
+
+    public int getLevel() {
+        return level;
     }
 
     public static Priority getByValue(String value) {
