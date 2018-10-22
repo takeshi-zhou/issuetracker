21e568879baf3be885e6b2a4c932ac25b7637fbf
wzy <2410685592@qq.com>
2018-10-22 20:23:58 +0800

    log configuration

--- project-manager/src/test/java/cn/edu/fudan/projectmanager/ProjectManagerApplicationTests.java
+++ project-manager/src/test/java/cn/edu/fudan/projectmanager/ProjectManagerApplicationTests.java
@@ -2,17 +2,11 @@ package cn.edu.fudan.projectmanager;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.modules.junit4.PowerMockRunnerDelegate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.SessionCallback;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -32,32 +26,6 @@ public class ProjectManagerApplicationTests {
     @AfterClass
     public static void afterTest() {
         System.out.println("结束测试..................................");
-    }
-
-    @Autowired
-    private StringRedisTemplate stringRedisTemplate;
-
-    @Test
-    @SuppressWarnings("unchecked")
-    public void test(){
-        stringRedisTemplate.execute(new SessionCallback<Object>() {
-            @Override
-            public Object execute(RedisOperations operations) throws DataAccessException {
-                operations.multi();
-                operations.delete("test");
-                return operations.exec();
-            }
-        });
-    }
-
-    @Test
-    public void test2(){
-        //redisTemplate使用事务前必须开启事务支持
-        stringRedisTemplate.setEnableTransactionSupport(true);
-        stringRedisTemplate.multi();
-        stringRedisTemplate.opsForValue().set("test","test");
-        stringRedisTemplate.exec();
-        System.out.println(stringRedisTemplate.opsForValue().get("test"));
     }
 
 }
