191a21f8cdc792b2830d2402eb5ae2d1ca09685e
wzy <2410685592@qq.com>
2018-11-02 18:46:17 +0800

    add multi environment

--- tag-service/src/main/java/cn/edu/fudan/tagservice/TagServiceApplication.java
+++ tag-service/src/main/java/cn/edu/fudan/tagservice/TagServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@PropertySource("classpath:conf.properties")
 @MapperScan("cn.edu.fudan.tagservice.mapper")
 @EnableTransactionManagement//开启事务支持
 @EnableAsync//开启异步调用的支持
