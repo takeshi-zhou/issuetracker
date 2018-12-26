c7d8289f5ef2bca772ce1bdff204a76d4d9983d4
pdtx <2461812105@qq.com>
2018-12-26 16:24:01 +0800

    modify excuteShell

--- scan-service/src/main/java/cn/edu/fudan/scanservice/util/ExecuteShellUtil.java
+++ scan-service/src/main/java/cn/edu/fudan/scanservice/util/ExecuteShellUtil.java
@@ -4,16 +4,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 @Component
 public class ExecuteShellUtil {
 
-    @Value("${workHome}")
-    private String workHome;
-
     @Value("${repoHome}")
     private String repoHome;
+
+    @Value("${binHome}")
+    private String binHome;
 
     public  boolean executeAnalyse(String repoPath, String projectName) {
         try {
@@ -21,7 +22,7 @@ public class ExecuteShellUtil {
             //String findbugs = "findbugs -xml -output  /home/fdse/issueTracker/resultfile/" + projectName
             //		+ ".xml " + repoPath;
             //脚本实现 用来解耦 还需要与tool解耦合
-            String command=workHome+"executeTools.sh " + projectName + " " + repoPath;
+            String command = binHome + "executeTools.sh " + projectName + " " + repoPath;
             System.out.println("command -> "+command);
             Process process = rt.exec(command);
             int exitValue = process.waitFor();
@@ -36,7 +37,7 @@ public class ExecuteShellUtil {
     public  boolean executeMvn(String repoPath) {
         try {
             Runtime rt = Runtime.getRuntime();
-            String command = workHome+"executeMvn.sh " + repoPath;
+            String command = binHome + "executeMvn.sh " + repoPath;
             System.out.println("command -> "+command);
             Process process = rt.exec(command);
             int exitValue = process.waitFor();
@@ -48,11 +49,11 @@ public class ExecuteShellUtil {
         return false;
     }
 
-    //do not include/consider the situation that there are at least files holding the same name in project-home dir
     public  String getFileLocation(String repoPath, String fileName) {
         try {
             Runtime rt = Runtime.getRuntime();
-            String command = "find "+repoHome + repoPath + " -name " + fileName;
+            //String command = "find "+repoHome + repoPath + " -name " + fileName;
+            String command =  binHome + "findOneFile.sh " + repoHome + repoPath + " "+ fileName;
             System.out.println("command -> "+command);
             Process process = rt.exec(command);
             process.waitFor();
