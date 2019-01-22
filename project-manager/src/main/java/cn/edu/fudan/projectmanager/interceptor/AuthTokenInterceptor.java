0f81cc38f63b375b313a51deaa864aa0ea069283
pdtx <2461812105@qq.com>
2019-01-22 17:29:34 +0800

    fix HRS_REQUEST_PARAMETER_TO_HTTP_HEADER  bug

--- project-manager/src/main/java/cn/edu/fudan/projectmanager/interceptor/AuthTokenInterceptor.java
+++ project-manager/src/main/java/cn/edu/fudan/projectmanager/interceptor/AuthTokenInterceptor.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.HtmlUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -30,6 +31,7 @@ public class AuthTokenInterceptor implements HandlerInterceptor {
         httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
         httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE");
         String requestHeaders = httpServletRequest.getHeader("Access-Control-Request-Headers");
+        requestHeaders = HtmlUtils.htmlEscape(requestHeaders);
         httpServletResponse.setHeader("Access-Control-Allow-Headers", requestHeaders);
         // 跨域时会首先发送一个option请求，该请求不会携带header 这里我们给option请求直接返回正常状态
         if (httpServletRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
--- scan-service/src/main/java/cn/edu/fudan/scanservice/interceptor/AuthTokenInterceptor.java
+++ scan-service/src/main/java/cn/edu/fudan/scanservice/interceptor/AuthTokenInterceptor.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.HtmlUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -36,6 +37,8 @@ public class AuthTokenInterceptor implements HandlerInterceptor {
         httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
         httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE");
         String requestHeaders = httpServletRequest.getHeader("Access-Control-Request-Headers");
+        // XSS 过滤
+        requestHeaders = HtmlUtils.htmlEscape(requestHeaders);
         httpServletResponse.setHeader("Access-Control-Allow-Headers", requestHeaders);
         // 跨域时会首先发送一个option请求，该请求不会携带header 这里我们给option请求直接返回正常状态
         if (httpServletRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
