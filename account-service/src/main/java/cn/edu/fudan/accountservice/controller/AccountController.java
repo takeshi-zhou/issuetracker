84878be8669a3066f922f8a3054c465327dbbbff
linwangzai <957859199@qq.com>
2019-03-20 21:35:20 +0800

    adjust to CAT

--- account-service/src/main/java/cn/edu/fudan/accountservice/controller/AccountController.java
+++ account-service/src/main/java/cn/edu/fudan/accountservice/controller/AccountController.java
@@ -1,9 +1,12 @@
 package cn.edu.fudan.accountservice.controller;
 
+import cn.edu.fudan.accountservice.component.cat.CatHttpRequestTransaction;
 import cn.edu.fudan.accountservice.domain.Account;
 import cn.edu.fudan.accountservice.domain.ResponseBean;
 import cn.edu.fudan.accountservice.service.AccountService;
 import cn.edu.fudan.accountservice.util.CookieUtil;
+import com.dianping.cat.Cat;
+import com.dianping.cat.message.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,24 +26,28 @@ public class AccountController {
 
     @GetMapping("/account-name/check")
     @CrossOrigin
+    @CatHttpRequestTransaction(type = "accountURL", name = "/user/account-name/check")
     public Object checkUserName(@RequestParam("accountName") String accountName) {
         return new ResponseBean(200, "success", accountService.isAccountNameExist(accountName));
     }
 
     @GetMapping("/email/check")
     @CrossOrigin
+    @CatHttpRequestTransaction(type = "accountURL", name = "/user/email/check")
     public Object checkEmail(@RequestParam("email") String email) {
         return new ResponseBean(200, "success", accountService.isEmailExist(email));
     }
 
     @GetMapping("/nick-name/check")
     @CrossOrigin
+    @CatHttpRequestTransaction(type = "accountURL", name = "/user/nick-name/check")
     public Object checkNickName(@RequestParam("nickName") String nickName) {
         return new ResponseBean(200, "success", accountService.isNameExist(nickName));
     }
 
     @PostMapping("/register")
     @CrossOrigin
+    @CatHttpRequestTransaction(type = "accountURL", name = "/user/register")
     public Object createUser(@RequestBody Account account) {
         try {
             accountService.addAccount(account);
@@ -52,6 +59,7 @@ public class AccountController {
 
     @GetMapping(value = {"/login"})
     @CrossOrigin
+    @CatHttpRequestTransaction(type = "accountURL", name = "/user/login")
     public Object login(@RequestParam("username") String username, @RequestParam("password") String password, HttpServletResponse response) {
         ResponseBean responseBean = accountService.login(username, password);
         if (responseBean.getData() != null) {
@@ -62,12 +70,14 @@ public class AccountController {
 
     @GetMapping(value = "/accountId")
     @CrossOrigin
+    @CatHttpRequestTransaction(type = "accountURL", name = "/user/accountId")
     public Object getAccountID(@RequestParam("userToken") String userToken) {
         return accountService.getAccountByToken(userToken).getUuid();
     }
 
     @GetMapping(value = "/auth/{userToken}")
     @CrossOrigin
+    @CatHttpRequestTransaction(type = "accountURL")
     public Object auth(@PathVariable("userToken") String userToken) {
         if (accountService.authByToken(userToken)) {
             return new ResponseBean(200, "auth pass", null);
@@ -77,6 +87,7 @@ public class AccountController {
     }
 
     @GetMapping(value = "/accountIds")
+    @CatHttpRequestTransaction(type = "accountURL", name = "/user/accountIds")
     public Object getAccountIds() {
         return accountService.getAllAccountId();
     }
