7ceb329602c522685b9cf122b0fc46465c088761
SouthStreet <znj764359824@gmail.com>
2019-03-25 19:26:37 +0800

    Add ScanSatusDao

--- clone-service/src/main/java/cn/edu/fudan/cloneservice/dao/PackageScanStatusDao.java
+++ clone-service/src/main/java/cn/edu/fudan/cloneservice/dao/PackageScanStatusDao.java
@@ -1,11 +1,16 @@
 package cn.edu.fudan.cloneservice.dao;
 
 import cn.edu.fudan.cloneservice.mapper.PackageScanStatusMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public class PackageScanStatusDao {
-    private PackageScanStatusMapper packageScanStatusMapper;
+
+
+    PackageScanStatusMapper packageScanStatusMapper;
 
 }
--- clone-service/src/main/java/cn/edu/fudan/cloneservice/mapper/PackageScanStatusMapper.java
+++ clone-service/src/main/java/cn/edu/fudan/cloneservice/mapper/PackageScanStatusMapper.java
@@ -1,8 +1,18 @@
 package cn.edu.fudan.cloneservice.mapper;
 
-import org.springframework.stereotype.Repository;
+import cn.edu.fudan.cloneservice.domain.PackageScanStatus;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
-@Repository
+import java.util.List;
+
+
+@Mapper
 public interface PackageScanStatusMapper {
+
+    @Select("select * from ce_scan_status where repo_id = #{p_repo_id} and p_commit_id = #{p_commit_id}")
+    PackageScanStatus selectPackageScanStatus(@Param("p_repo_id") String repo_id,
+                                              @Param("p_commit_id") String commit_id);
 
 }
