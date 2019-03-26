ed9c8d41db9dfa3689ddc30b93ca78d360fc3f24
pdtx <2461812105@qq.com>
2019-03-26 17:21:47 +0800

    modify recommendation module

---
+++ issuetracker/src/main/java/cn/edu/fudan/issuetracker/util/CodeCompare.java
@@ -0,0 +1,5 @@
+package cn.edu.fudan.issuetracker.util;
+
+public interface CodeCompare {
+
+}
---
+++ recommendation/src/main/java/cn/edu/fudan/recommendation/dao/RecommendationDao.java
@@ -0,0 +1,49 @@
+package cn.edu.fudan.recommendation.dao;
+
+import cn.edu.fudan.recommendation.domain.BugRecommendation;
+import cn.edu.fudan.recommendation.mapper.RecommendationMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RecommendationDao {
+
+    private RecommendationMapper recommendationMapper;
+
+    @Autowired
+    public void setRecommendationMapper(RecommendationMapper recommendationMapper){
+        this.recommendationMapper = recommendationMapper;
+    }
+
+    public void addBugRecommendation(BugRecommendation recommendation){
+        recommendationMapper.addBugRecommendation(recommendation);
+    }
+
+    public boolean isLocationExist(String location){
+        return getRecommendationByLocation(location) != null;
+    }
+
+    public boolean isTypeExist(String type){
+        return getRecommendationsByType(type).size()!=0;
+    }
+
+//    public BugRecommendation getRecommendationByType(String type){
+//        return recommendationMapper.getRecommendationByType(type);
+//    }
+
+    public List<BugRecommendation> getRecommendationsByType(String type) {
+        List<BugRecommendation> recommendations = recommendationMapper.getRecommendationsByType(type);
+        List<BugRecommendation> list = new ArrayList<>();
+        for (BugRecommendation recommendation : recommendations) {
+            list.add(recommendation);
+        }
+        return list;
+    }
+
+    public BugRecommendation getRecommendationByLocation(String location){
+        return recommendationMapper.getRecommendationByLocation(location);
+    }
+}
