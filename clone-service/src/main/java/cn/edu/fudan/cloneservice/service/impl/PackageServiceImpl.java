1a491633c8f24017de73d36b3c191b088a95b1fa
SouthStreet <znj764359824@gmail.com>
2019-04-22 15:21:26 +0800

    change log home &  confige server debug

--- clone-service/src/main/java/cn/edu/fudan/cloneservice/service/impl/PackageServiceImpl.java
+++ clone-service/src/main/java/cn/edu/fudan/cloneservice/service/impl/PackageServiceImpl.java
@@ -46,13 +46,11 @@ public class PackageServiceImpl implements PackageService {
         //2 repo 名字
         //3 repo 路径
         String repoId=scanInitialInfo.getRepoId();
-        String repoName=scanInitialInfo.getRepoName();
-        String repoPath=scanInitialInfo.getRepoPath();
         List<String> commitList = scanInitialInfo.getCommitList();
         //#1 根据给的信息，启动包的扫描服务
         //把这个对象信息填充好 写一个task并run
 
-        packageScanTask.run(repoId,repoName,repoPath,commitList);
+        packageScanTask.run(repoId,commitList);
 
     }
 
