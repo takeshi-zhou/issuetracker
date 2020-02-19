81827b2b7472554dcc53cbbfe4966b74fe06fda8
bfkh <736759961@qq.com>
2020-02-19 17:17:21 +0800

    Update deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -1,1 +1,3 @@
-scp $(find $(pwd) -name *-*-SNAPSHOT.jar) root@10.141.221.85:/home/fdse/user/issueTracker/jenkins
+scp $(find $(pwd) -name *-*-SNAPSHOT.jar) root@10.141.221.85:/home/fdse/user/issueTracker/
+
+ssh root@10.141.221.85 "cd /home/fdse/user/issueTracker/ ; ./issueTracker-demo.sh stop ; sleep 2 ; ./issueTracker-demo.sh start external"
