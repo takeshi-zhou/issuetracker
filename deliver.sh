685e9f9f5969dceec4603dca501aa378ebe7e8f4
bfkh <736759961@qq.com>
2020-02-20 14:49:13 +0800

    Update deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -1,3 +1,3 @@
-scp $(find $(pwd) -name *-*-SNAPSHOT.jar) root@10.141.221.85:/home/fdse/user/issueTracker/
+scp $(find $(pwd) -name *-*-SNAPSHOT.jar) fdse@10.141.221.85:/home/fdse/user/issueTracker/
 
-ssh root@10.141.221.85 "cd /home/fdse/user/issueTracker/ ; ./issueTracker-demo.sh stop ; sleep 2 ; ./issueTracker-demo.sh start external"
+ssh fdse@10.141.221.85 "cd /home/fdse/user/issueTracker/ ; ./issueTracker-demo.sh stop ; sleep 2 ; ./issueTracker-demo.sh start external"
