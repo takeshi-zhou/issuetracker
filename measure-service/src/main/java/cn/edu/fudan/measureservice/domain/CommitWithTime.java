b750a6677acf7e61bb43ff538515d3294a928722
jfstylejf <15801850335@163.com>
2020-04-28 18:28:12 +0800

    change CommitWithTime class

--- measure-service/src/main/java/cn/edu/fudan/measureservice/domain/CommitWithTime.java
+++ measure-service/src/main/java/cn/edu/fudan/measureservice/domain/CommitWithTime.java
@@ -1,14 +1,49 @@
 package cn.edu.fudan.measureservice.domain;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
 public class CommitWithTime {
     private String repoId;
     private String commitId;
     private String commitTime;
     private String developerName;
     private String developerEmail;
+
+    public String getRepoId() {
+        return repoId;
+    }
+
+    public void setRepoId(String repoId) {
+        this.repoId = repoId;
+    }
+
+    public String getCommitId() {
+        return commitId;
+    }
+
+    public void setCommitId(String commitId) {
+        this.commitId = commitId;
+    }
+
+    public String getCommitTime() {
+        return commitTime;
+    }
+
+    public void setCommitTime(String commitTime) {
+        this.commitTime = commitTime;
+    }
+
+    public String getDeveloperName() {
+        return developerName;
+    }
+
+    public void setDeveloperName(String developerName) {
+        this.developerName = developerName;
+    }
+
+    public String getDeveloperEmail() {
+        return developerEmail;
+    }
+
+    public void setDeveloperEmail(String developerEmail) {
+        this.developerEmail = developerEmail;
+    }
 }
